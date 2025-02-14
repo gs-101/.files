da875477406a9bdaf27338247f640e424bebda5f
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-02-14 13:56:08 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,50 +1,50 @@
 (list (channel
-       (name 'nonguix)
-       (url "https://gitlab.com/nonguix/nonguix")
-       (branch "master")
-       (commit
-        "bd2f475ac64b3c7e2b4f12fa90f617cbee1028e2")
-       (introduction
-        (make-channel-introduction
-         "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
-         (openpgp-fingerprint
-          "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "9f06b18aefd1e2648352ccdc51c86204a7cf5e87")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
       (channel
-       (name 'ollama-guix)
-       (url "https://codeberg.org/tusharhero/ollama-guix.git")
-       (branch "master")
-       (commit
-        "7a73edd21a71ca0a7ad43f491e2754537acbb8d6"))
+        (name 'ollama-guix)
+        (url "https://codeberg.org/tusharhero/ollama-guix.git")
+        (branch "master")
+        (commit
+          "7a73edd21a71ca0a7ad43f491e2754537acbb8d6"))
       (channel
-       (name 'emacs-master)
-       (url "https://github.com/gs-101/emacs-master.git")
-       (branch "main")
-       (commit
-        "caf0fdf59de2e6ae4ef919bdfe3b12cfd7b69e4f")
-       (introduction
-        (make-channel-introduction
-         "568579841d0ca41a9d222a2cfcad9a7367f9073b"
-         (openpgp-fingerprint
-          "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
+        (name 'emacs-master)
+        (url "https://github.com/gs-101/emacs-master.git")
+        (branch "main")
+        (commit
+          "7ab2caf5ba732d10ba882452bc3bd941ec598d27")
+        (introduction
+          (make-channel-introduction
+            "568579841d0ca41a9d222a2cfcad9a7367f9073b"
+            (openpgp-fingerprint
+              "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
       (channel
-       (name 'selected-guix-works)
-       (url "https://github.com/gs-101/selected-guix-works.git")
-       (branch "main")
-       (commit
-        "89074834a1df570de061f7d731f28d4506a611d7")
-       (introduction
-        (make-channel-introduction
-         "5d1270d51c64457d61cd46ec96e5599176f315a4"
-         (openpgp-fingerprint
-          "C780 21F7 34E4 07EB 9090  0CF1 4ACA 6D6F 89AB 3162"))))
+        (name 'selected-guix-works)
+        (url "https://github.com/gs-101/selected-guix-works.git")
+        (branch "main")
+        (commit
+          "23f5af034b202e2dabe3f1e80da2e169ff2c6ed7")
+        (introduction
+          (make-channel-introduction
+            "5d1270d51c64457d61cd46ec96e5599176f315a4"
+            (openpgp-fingerprint
+              "C780 21F7 34E4 07EB 9090  0CF1 4ACA 6D6F 89AB 3162"))))
       (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (branch "master")
-       (commit
-        "c9abfacd9f58f7365668b67a8125640b7497932a")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "93551174af7a96d61f22f013ad1b9935106bf040")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
