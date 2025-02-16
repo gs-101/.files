f5e8f127b7d55a086169502e3efdbd378522f34a
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-02-16 12:22:43 -0300

    style(guix/system/guix-pc.scm): indent file

--- guix/system/guix-pc.scm
+++ guix/system/guix-pc.scm
@@ -7,7 +7,7 @@
                      xorg
                      containers)
 
-(define %gs-101/desktop-services  
+(define %gs-101/desktop-services
   (modify-services %desktop-services
     (delete gdm-service-type)
     (guix-service-type config => (guix-configuration
@@ -24,35 +24,35 @@
                                            %default-authorized-guix-keys))))))
 
 (operating-system
- (kernel (specification->package "linux"))
- (initrd microcode-initrd)
- (firmware (map specification->package
-                '("linux-firmware")))
- (locale "en_US.utf8")
- (timezone "America/Sao_Paulo")
- (keyboard-layout (keyboard-layout "br"
-                                   #:options '("ctrl:nocaps")))
- (host-name "guix-pc")
+  (kernel (specification->package "linux"))
+  (initrd microcode-initrd)
+  (firmware (map specification->package
+                 '("linux-firmware")))
+  (locale "en_US.utf8")
+  (timezone "America/Sao_Paulo")
+  (keyboard-layout (keyboard-layout "br"
+                                    #:options '("ctrl:nocaps")))
+  (host-name "guix-pc")
 
- (users (cons* (user-account
-                (name "gabriel")
-                (comment "Gabriel Santos")
-                (group "users")
-                (home-directory "/home/gabriel")
-                (supplementary-groups '("wheel"
-                                        "netdev"
-                                        "audio"
-                                        "video")))
-               %base-user-accounts))
+  (users (cons* (user-account
+                 (name "gabriel")
+                 (comment "Gabriel Santos")
+                 (group "users")
+                 (home-directory "/home/gabriel")
+                 (supplementary-groups '("wheel"
+                                         "netdev"
+                                         "audio"
+                                         "video")))
+                %base-user-accounts))
 
- (packages (append
-            (map specification->package+output
-                 '("git"
-                   "nix"
-                   "direnv"))
-            %base-packages))
+  (packages (append
+             (map specification->package+output
+                  '("git"
+                    "nix"
+                    "direnv"))
+             %base-packages))
 
- (issue "
+  (issue "
  ..                             `.
  `--..```..`           `..```..--`
    .-:///-:::.       `-:::///:-.
@@ -65,24 +65,24 @@
               `-....`
 ")
 
- (services (cons* (service cups-service-type)
-                  (service nix-service-type)
-                  (service rootless-podman-service-type)
-                  %gs-101/desktop-services))
+  (services (cons* (service cups-service-type)
+                   (service nix-service-type)
+                   (service rootless-podman-service-type)
+                   %gs-101/desktop-services))
 
- (bootloader (bootloader-configuration
-              (bootloader grub-efi-bootloader)
-              (targets (list "/boot/efi"))
-              (keyboard-layout keyboard-layout)))
+  (bootloader (bootloader-configuration
+               (bootloader grub-efi-bootloader)
+               (targets (list "/boot/efi"))
+               (keyboard-layout keyboard-layout)))
 
- (file-systems (cons* (file-system
-                       (mount-point "/")
-                       (device (uuid
-                                "32f6f5f3-040b-419a-a90b-11207887cd41"
-                                'btrfs))
-                       (type "btrfs"))
-                      (file-system
-                       (mount-point "/boot/efi")
-                       (device (uuid "6C59-0ADD"
-                                     'fat32))
-                       (type "vfat")) %base-file-systems)))
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "32f6f5f3-040b-419a-a90b-11207887cd41"
+                                  'btrfs))
+                         (type "btrfs"))
+                       (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "6C59-0ADD"
+                                       'fat32))
+                         (type "vfat")) %base-file-systems)))
