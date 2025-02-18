ee1c031fec75f804d6a82de0403259e19d2cef3e
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-02-18 13:26:28 -0300

    style(guix/home/guix-pc.scm): indent file

--- guix/home/guix-pc.scm
+++ guix/home/guix-pc.scm
@@ -13,46 +13,46 @@
              (selected-guix-works utils))
 
 (home-environment
-  (services
-   (let ((DOTFILES ".files/"))
-     (cons* (simple-service 'home-variables-service
-                            home-environment-variables-service-type
-                            `(("DOTFILES" . "$HOME/.files")
-                              ("TERM" . "kitty")
-                              ("EDITOR" . "emacsclient -nw -a 'emacs -nw'")
-                              ("VISUAL" . "emacsclient --create-frame -a 'emacs'")
-                              ("SHELL" . "bash")
-                              ("BROWSER" . "librewolf")
-                              ("FILE_MANAGER" . "tv")
-                              ("BAR" . "waybar")
-                              ("MENU" . "fuzzel")))
-            (service home-bash-service-type
-                     (home-bash-configuration
-                      (environment-variables '(("HISTCONTROL" . "ignoredups:erasedups")))
-                      (aliases '(("grep" . "grep -P -i -n -H -q -R --devices=read --color=auto")
-                                 (".." . "cd ..")
-                                 ("cat" . "bat --color auto --decorations auto --paging never")
-                                 ("ls" . "eza -l -X --color auto --icons auto --hyperlink -a --group-directories-first --smart-group -h --changed --git")
-                                 ("ip" . "ip -color=auto")
-                                 ("df" . "df -H")
-                                 ("free" . "free -m -h --si")
-                                 ("docker" . "podman")
-                                 ("find" . "fd")))
-                      (bash-profile (list (local-file (string-append DOTFILES "bash/.bash_profile")
-                                                      "bash_profile")))))
-            (simple-service 'home-bash-extension-service
-                            home-bash-service-type
-                            (home-bash-extension
-                             (bash-profile (list (plain-file "bash_profile"
-                                                             "
+ (services
+  (let ((DOTFILES ".files/"))
+    (cons* (simple-service 'home-variables-service
+                           home-environment-variables-service-type
+                           `(("DOTFILES" . "$HOME/.files")
+                             ("TERM" . "kitty")
+                             ("EDITOR" . "emacsclient -nw -a 'emacs -nw'")
+                             ("VISUAL" . "emacsclient --create-frame -a 'emacs'")
+                             ("SHELL" . "bash")
+                             ("BROWSER" . "librewolf")
+                             ("FILE_MANAGER" . "tv")
+                             ("BAR" . "waybar")
+                             ("MENU" . "fuzzel")))
+           (service home-bash-service-type
+                    (home-bash-configuration
+                     (environment-variables '(("HISTCONTROL" . "ignoredups:erasedups")))
+                     (aliases '(("grep" . "grep -P -i -n -H -q -R --devices=read --color=auto")
+                                (".." . "cd ..")
+                                ("cat" . "bat --color auto --decorations auto --paging never")
+                                ("ls" . "eza -l -X --color auto --icons auto --hyperlink -a --group-directories-first --smart-group -h --changed --git")
+                                ("ip" . "ip -color=auto")
+                                ("df" . "df -H")
+                                ("free" . "free -m -h --si")
+                                ("docker" . "podman")
+                                ("find" . "fd")))
+                     (bash-profile (list (local-file (string-append DOTFILES "bash/.bash_profile")
+                                                     "bash_profile")))))
+           (simple-service 'home-bash-extension-service
+                           home-bash-service-type
+                           (home-bash-extension
+                            (bash-profile (list (plain-file "bash_profile"
+                                                            "
 # Set up Nix profile.
 source /run/current-system/profile/etc/profile.d/nix.sh
 
 # Keep Home Manager out of shell configuration.
 if [ -f ~/.nix-profile/bin/home-manager ]; then source ~/.nix-profile/etc/profile.d/hm-session-vars.sh; fi
 ")))
-                             (bashrc (list (plain-file "bashrc"
-                                                       "
+                            (bashrc (list (plain-file "bashrc"
+                                                      "
 # Define the GPG_TTY as the one being currently used.
 # This fixes so many issues that it should be a default.
 export GPG_TTY=$(tty)
@@ -68,122 +68,122 @@ shopt -s histappend
 # Recursive globbing.
 shopt -s globstar
 ")))))
-            (service home-inputrc-service-type
-                     (home-inputrc-configuration
-                      (variables
-                       '(("editing-mode" . "emacs")))))
-            (service home-gpg-agent-service-type
-                     (home-gpg-agent-configuration
-                      (pinentry-program
-                       (file-append pinentry-qt "/bin/pinentry-qt"))
-                      (ssh-support? #t)))
-            (home-profile-package-service "font-microsoft-cascadia-next")
-            (home-profile-package-service "font-google-noto-emoji")
-            (home-profile-package-service "font-aporetic")
-            (simple-service 'home-fontconfig-service
-                            home-fontconfig-service-type
-                            (list "~/.guix-home/profile/share/fonts/"
-                                  '(alias
-                                    (family "monospace")
-                                    (prefer
-                                     (family "Cascadia Mono NF")))
-                                  '(alias
-                                    (family "serif")
-                                    (prefer
-                                     (family "Aporetic Serif")))
-                                  '(alias
-                                    (family "sans-serif")
-                                    (prefer
-                                     (family "Cascadia Code NF")))
-                                  '(alias
-                                    (family "emoji")
-                                    (prefer
-                                     (family "Noto Color Emoji")))))
-            (simple-service 'home-openssh-service
-                            home-profile-service-type
-                            (list (specification->package "openssh")))
-            (service home-syncthing-service-type
-                     (for-home
-                      (syncthing-configuration
-                       (user "gabriel"))))
-            (service home-dbus-service-type)
-            (service home-pipewire-service-type)
-            (simple-service 'home-waybar-configuration-service
-                            home-xdg-configuration-files-service-type
-                            (list `("waybar"
-                                    ,(local-file (string-append DOTFILES "waybar/")
-                                                 "waybar"
-                                                 #:recursive? #t))))
-            (simple-service 'home-nix-configuration-service
-                            home-xdg-configuration-files-service-type
-                            (list `("nix/nix.conf"
-                                    ,(plain-file "nix.conf"
-                                                 "experimental-features = nix-command flakes"))))
-            (simple-service 'home-nix-home-manager-service
-                            home-xdg-configuration-files-service-type
-                            (list `("home-manager"
-                                    ,(local-file (string-append DOTFILES "nix/home/")
-                                                 "home-manager"
-                                                 #:recursive? #t))))
-            (simple-service 'home-makefile-service
-                            home-files-service-type
-                            (list `("Makefile"
-                                    ,(local-file (string-append DOTFILES "Makefile")
-                                                 "Makefile"))))
-            (simple-service 'home-gitignore-service
-                            home-files-service-type
-                            (list `(".gitignore"
-                                    ,(plain-file "gitignore" "*.elc
+           (service home-inputrc-service-type
+                    (home-inputrc-configuration
+                     (variables
+                      '(("editing-mode" . "emacs")))))
+           (service home-gpg-agent-service-type
+                    (home-gpg-agent-configuration
+                     (pinentry-program
+                      (file-append pinentry-qt "/bin/pinentry-qt"))
+                     (ssh-support? #t)))
+           (home-profile-package-service "font-microsoft-cascadia-next")
+           (home-profile-package-service "font-google-noto-emoji")
+           (home-profile-package-service "font-aporetic")
+           (simple-service 'home-fontconfig-service
+                           home-fontconfig-service-type
+                           (list "~/.guix-home/profile/share/fonts/"
+                                 '(alias
+                                   (family "monospace")
+                                   (prefer
+                                    (family "Cascadia Mono NF")))
+                                 '(alias
+                                   (family "serif")
+                                   (prefer
+                                    (family "Aporetic Serif")))
+                                 '(alias
+                                   (family "sans-serif")
+                                   (prefer
+                                    (family "Cascadia Code NF")))
+                                 '(alias
+                                   (family "emoji")
+                                   (prefer
+                                    (family "Noto Color Emoji")))))
+           (simple-service 'home-openssh-service
+                           home-profile-service-type
+                           (list (specification->package "openssh")))
+           (service home-syncthing-service-type
+                    (for-home
+                     (syncthing-configuration
+                      (user "gabriel"))))
+           (service home-dbus-service-type)
+           (service home-pipewire-service-type)
+           (simple-service 'home-waybar-configuration-service
+                           home-xdg-configuration-files-service-type
+                           (list `("waybar"
+                                   ,(local-file (string-append DOTFILES "waybar/")
+                                                "waybar"
+                                                #:recursive? #t))))
+           (simple-service 'home-nix-configuration-service
+                           home-xdg-configuration-files-service-type
+                           (list `("nix/nix.conf"
+                                   ,(plain-file "nix.conf"
+                                                "experimental-features = nix-command flakes"))))
+           (simple-service 'home-nix-home-manager-service
+                           home-xdg-configuration-files-service-type
+                           (list `("home-manager"
+                                   ,(local-file (string-append DOTFILES "nix/home/")
+                                                "home-manager"
+                                                #:recursive? #t))))
+           (simple-service 'home-makefile-service
+                           home-files-service-type
+                           (list `("Makefile"
+                                   ,(local-file (string-append DOTFILES "Makefile")
+                                                "Makefile"))))
+           (simple-service 'home-gitignore-service
+                           home-files-service-type
+                           (list `(".gitignore"
+                                   ,(plain-file "gitignore" "*.elc
 *-autoloads.el
 *-pkg.el
 *.DS_Store"))))
-            (simple-service 'home-wakatime-service
-                            home-files-service-type
-                            (list `(".wakatime.cfg"
-                                    ,(local-file (string-append DOTFILES "wakatime/.wakatime.cfg")
-                                                 "wakatime.cfg"))))
-            (home-profile-package-service "emacs-master-pgtk")
-            (home-profile-package-service "neovim")
-            (home-profile-package-service "password-store")
-            (home-profile-package-service "bat")
-            (home-profile-package-service "eza")
-            (home-profile-package-service "ripgrep")
-            (home-profile-package-service "fuzzel")
-            (home-profile-package-service "kitty")
-            (home-profile-package-service "librewolf")
-            (home-profile-package-service "gnupg")
-            (home-profile-package-service '("hyprland"
-                                            "xdg-desktop-portal-hyprland"
-                                            "hyprpolkitagent"
-                                            "hyprpicker"
-                                            "hyprpaper"
-                                            ;; For hyprshot
-                                            "slurp"
-                                            "grim"
-                                            "libnotify"
-                                            "hyprshot"))
-            (home-profile-package-service "selected-guix-works-backgrounds")
-            (simple-service 'home-hyprpaper-configuration-service
-                            home-xdg-configuration-files-service-type
-                            (list `("hypr/hyprpaper.conf"
-                                    ,(plain-file "hyprpaper.conf"
-                                                 "
+           (simple-service 'home-wakatime-service
+                           home-files-service-type
+                           (list `(".wakatime.cfg"
+                                   ,(local-file (string-append DOTFILES "wakatime/.wakatime.cfg")
+                                                "wakatime.cfg"))))
+           (home-profile-package-service "emacs-master-pgtk")
+           (home-profile-package-service "neovim")
+           (home-profile-package-service "password-store")
+           (home-profile-package-service "bat")
+           (home-profile-package-service "eza")
+           (home-profile-package-service "ripgrep")
+           (home-profile-package-service "fuzzel")
+           (home-profile-package-service "kitty")
+           (home-profile-package-service "librewolf")
+           (home-profile-package-service "gnupg")
+           (home-profile-package-service '("hyprland"
+                                           "xdg-desktop-portal-hyprland"
+                                           "hyprpolkitagent"
+                                           "hyprpicker"
+                                           "hyprpaper"
+                                           ;; For hyprshot
+                                           "slurp"
+                                           "grim"
+                                           "libnotify"
+                                           "hyprshot"))
+           (home-profile-package-service "selected-guix-works-backgrounds")
+           (simple-service 'home-hyprpaper-configuration-service
+                           home-xdg-configuration-files-service-type
+                           (list `("hypr/hyprpaper.conf"
+                                   ,(plain-file "hyprpaper.conf"
+                                                "
 preload = /home/gabriel/.guix-home/profile/share/backgrounds/selected-guix-works/guix/guix-nix-checkered-16-9.png
 wallpaper = , /home/gabriel/.guix-home/profile/share/backgrounds/selected-guix-works/guix/guix-nix-checkered-16-9.png
 "))))
-            (simple-service 'home-hyprland-configuration-service
-                            home-xdg-configuration-files-service-type
-                            (list `("hypr/hyprland.conf"
-                                    ,(local-file (string-append DOTFILES "hypr/hyprland.conf")
-                                                 "hyprland.conf"))))
-            (home-profile-package-service "fnott")
-            (home-profile-package-service "waybar")
-            (home-profile-package-service "make")
-            (home-profile-package-service "mpv")
-            (home-profile-package-service "dolphin")
-            (home-profile-package-service '("wl-clipboard"
-                                            "clipman"))
-            (home-profile-package-service "udiskie")
-            (home-profile-package-service "bibata-cursor-theme")
-            (home-profile-package-service "fd")
-            %base-home-services))))
+           (simple-service 'home-hyprland-configuration-service
+                           home-xdg-configuration-files-service-type
+                           (list `("hypr/hyprland.conf"
+                                   ,(local-file (string-append DOTFILES "hypr/hyprland.conf")
+                                                "hyprland.conf"))))
+           (home-profile-package-service "fnott")
+           (home-profile-package-service "waybar")
+           (home-profile-package-service "make")
+           (home-profile-package-service "mpv")
+           (home-profile-package-service "dolphin")
+           (home-profile-package-service '("wl-clipboard"
+                                           "clipman"))
+           (home-profile-package-service "udiskie")
+           (home-profile-package-service "bibata-cursor-theme")
+           (home-profile-package-service "fd")
+           %base-home-services))))
