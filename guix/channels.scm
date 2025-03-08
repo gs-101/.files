e248f8fc1655ccc790dfa08cc3c04d1c791e36ae
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-07 21:20:41 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "d0b6099324cd47961064bd96ae830c4ca6d1bada")
+          "944619c1947ba59823f4ad6d8b13842b7e162d76")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -20,7 +20,7 @@
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "4a169b5656efcc088408c7935f644d7df102e1c3")
+          "95ec3b1c397192c4195db87c445b3ea195e678c2")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -31,18 +31,29 @@
         (url "https://github.com/gs-101/selected-guix-works.git")
         (branch "main")
         (commit
-          "02df36cda618793d6a0d2ed3631d0b7806ee924f")
+          "04acab4093eb32d7b803dac5f11da655cb36ac7e")
         (introduction
           (make-channel-introduction
             "5d1270d51c64457d61cd46ec96e5599176f315a4"
             (openpgp-fingerprint
               "C780 21F7 34E4 07EB 9090  0CF1 4ACA 6D6F 89AB 3162"))))
       (channel
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
+        (branch "master")
+        (commit
+          "8600655bbdc399b6fc682755d4d63f3709f562be")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "310adf4ce70cbb864859274fcc7842bd519bbddc")
+          "591d2492e8517220db57098a6c9d1242533a8c8f")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
