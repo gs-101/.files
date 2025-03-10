68125c55c545454a12e5cc6dadb2da3b07340fcf
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-10 10:46:47 -0300

    refactor(guix/system/guix-pc.scm): user groups
    * Organize groups alphabetically
    * Remove netdev (it was probably needed by another service in the
    default configuration)
    * Add cgroup for Podman

--- guix/system/guix-pc.scm
+++ guix/system/guix-pc.scm
@@ -21,30 +21,30 @@
                                            %default-authorized-guix-keys))))))
 
 (operating-system
- (kernel (specification->package "linux"))
- (initrd microcode-initrd)
- (firmware (map specification->package
-                '("linux-firmware")))
- (locale "en_US.utf8")
- (timezone "America/Sao_Paulo")
- (keyboard-layout (keyboard-layout "br"
-                                   #:options '("ctrl:nocaps")))
- (host-name "guix-pc")
+  (kernel (specification->package "linux"))
+  (initrd microcode-initrd)
+  (firmware (map specification->package
+                 '("linux-firmware")))
+  (locale "en_US.utf8")
+  (timezone "America/Sao_Paulo")
+  (keyboard-layout (keyboard-layout "br"
+                                    #:options '("ctrl:nocaps")))
+  (host-name "guix-pc")
 
- (users (cons* (user-account
-                (name "gabriel")
-                (comment "Gabriel Santos")
-                (group "users")
-                (home-directory "/home/gabriel")
-                (supplementary-groups '("wheel"
-                                        "netdev"
-                                        "audio"
-                                        "video")))
-               %base-user-accounts))
+  (users (cons* (user-account
+                 (name "gabriel")
+                 (comment "Gabriel Santos")
+                 (group "users")
+                 (home-directory "/home/gabriel")
+                 (supplementary-groups '("audio"
+                                         "cgroup" ; Podman
+                                         "video"
+                                         "wheel")))
+                %base-user-accounts))
 
- (packages %base-packages)
+  (packages %base-packages)
 
- (issue (colorize-string "
+  (issue (colorize-string "
  ..                             `.
  `--..```..`           `..```..--`
    .-:///-:::.       `-:::///:-.
@@ -57,24 +57,24 @@
               `-....`
 " (color CYAN)))
 
- (services (cons* (service cups-service-type)
-                  (service nix-service-type)
-                  (service rootless-podman-service-type)
-                  %gs-101/desktop-services))
+  (services (cons* (service cups-service-type)
+                   (service nix-service-type)
+                   (service rootless-podman-service-type)
+                   %gs-101/desktop-services))
 
- (bootloader (bootloader-configuration
-              (bootloader grub-efi-bootloader)
-              (targets (list "/boot/efi"))
-              (keyboard-layout keyboard-layout)))
+  (bootloader (bootloader-configuration
+               (bootloader grub-efi-bootloader)
+               (targets (list "/boot/efi"))
+               (keyboard-layout keyboard-layout)))
 
- (file-systems (cons* (file-system
-                       (mount-point "/")
-                       (device (uuid
-                                "32f6f5f3-040b-419a-a90b-11207887cd41"
-                                'btrfs))
-                       (type "btrfs"))
-                      (file-system
-                       (mount-point "/boot/efi")
-                       (device (uuid "6C59-0ADD"
-                                     'fat32))
-                       (type "vfat")) %base-file-systems)))
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "32f6f5f3-040b-419a-a90b-11207887cd41"
+                                  'btrfs))
+                         (type "btrfs"))
+                       (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "6C59-0ADD"
+                                       'fat32))
+                         (type "vfat")) %base-file-systems)))
