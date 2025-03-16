cf9655a11bcaa9e0926707a60abc4e2347eb6934
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-16 07:42:43 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -1,9 +1,53 @@
 (list (channel
+        (name 'nonguix)
+        (url "https://gitlab.com/nonguix/nonguix")
+        (branch "master")
+        (commit
+          "ac1f7b074ea47e8a330e3475732c2e905928df35")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'emacs-master)
+        (url "https://github.com/gs-101/emacs-master.git")
+        (branch "main")
+        (commit
+          "4f10e77ee8d2f4062cf4c5513171bb3874e7c45c")
+        (introduction
+          (make-channel-introduction
+            "568579841d0ca41a9d222a2cfcad9a7367f9073b"
+            (openpgp-fingerprint
+              "3049 BF6C 0829 94E4 38ED  4A15 3033 E0E9 F7E2 5FE4"))))
+      (channel
+        (name 'selected-guix-works)
+        (url "https://github.com/gs-101/selected-guix-works.git")
+        (branch "main")
+        (commit
+          "711f036b13673291602f32325bd344bf00881975")
+        (introduction
+          (make-channel-introduction
+            "5d1270d51c64457d61cd46ec96e5599176f315a4"
+            (openpgp-fingerprint
+              "C780 21F7 34E4 07EB 9090  0CF1 4ACA 6D6F 89AB 3162"))))
+      (channel
+        (name 'rde)
+        (url "https://git.sr.ht/~abcdw/rde")
+        (branch "master")
+        (commit
+          "08b963f50005023d5ec35bc828d0c2510221c83d")
+        (introduction
+          (make-channel-introduction
+            "257cebd587b66e4d865b3537a9a88cccd7107c95"
+            (openpgp-fingerprint
+              "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
+      (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "51c28379d64ab361473964f8f634224f0dfb24c1")
+          "3dc4c7d8c85e5afef43073a7c75a0bcb727dcb8f")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
