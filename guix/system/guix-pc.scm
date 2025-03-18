2fdc9aac786e2de0f6b75bf26b936b412e204fed
gs-101 <172639817+gs-101@users.noreply.github.com>
2025-03-18 17:29:08 -0300

    feat(guix/system/guix-pc.scm)!: sort everything I could alphabetically

--- guix/system/guix-pc.scm
+++ guix/system/guix-pc.scm
@@ -1,50 +1,47 @@
 (use-modules (gnu)
-             (nongnu system linux-initrd)
-             (guix colors))
+             (guix colors) ; For issue
+             (nongnu system linux-initrd))
 
-(use-service-modules cups
+(use-service-modules containers
+                     cups
                      desktop
                      nix
-                     xorg
-                     containers
-                     virtualization)
+                     virtualization
+                     xorg)
 
 (define %gs-101/desktop-services
   (modify-services %desktop-services
     (delete gdm-service-type)
     (guix-service-type config => (guix-configuration
                                   (inherit config)
-                                  (substitute-urls
-                                   (append (list "https://substitutes.nonguix.org")
-                                           %default-substitute-urls))
                                   (authorized-keys
                                    (append (list (local-file "../substitute-keys/nonguix.pub"))
-                                           %default-authorized-guix-keys))))))
+                                           %default-authorized-guix-keys))
+                                  (substitute-urls
+                                   (append (list "https://substitutes.nonguix.org")
+                                           %default-substitute-urls))))))
 
 (operating-system
-  (kernel (specification->package "linux"))
-  (initrd microcode-initrd)
-  (firmware (map specification->package
-                 '("linux-firmware")))
-  (locale "en_US.utf8")
-  (timezone "America/Sao_Paulo")
-  (keyboard-layout (keyboard-layout "br"
-                                    #:options '("ctrl:nocaps")))
+  (bootloader
+   (bootloader-configuration
+    (bootloader grub-efi-bootloader)
+    (keyboard-layout keyboard-layout)
+    (targets (list "/boot/efi"))))
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "32f6f5f3-040b-419a-a90b-11207887cd41"
+                                  'btrfs))
+                         (type "btrfs"))
+                       (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "6C59-0ADD"
+                                       'fat32))
+                         (type "vfat"))
+                       %base-file-systems))
+  (firmware (specifications->packages '("linux-firmware")))
   (host-name "guix-pc")
-
-  (users (cons* (user-account
-                 (name "gabriel")
-                 (comment "Gabriel Santos")
-                 (group "users")
-                 (home-directory "/home/gabriel")
-                 (supplementary-groups '("audio"
-                                         "cgroup" ; Podman
-                                         "video"
-                                         "wheel")))
-                %base-user-accounts))
-
-  (packages %base-packages)
-
+  (initrd microcode-initrd)
   (issue (colorize-string "
  ..                             `.
  `--..```..`           `..```..--`
@@ -57,7 +54,10 @@
               :+/:::-
               `-....`
 " (color CYAN)))
-
+  (kernel (specification->package "linux"))
+  (keyboard-layout (keyboard-layout "br" #:options '("ctrl:nocaps")))
+  (locale "en_US.utf8")
+  (packages %base-packages)
   (services (cons* (service cups-service-type)
                    (service nix-service-type)
                    (service qemu-binfmt-service-type
@@ -65,20 +65,14 @@
                              (platforms (lookup-qemu-platforms "arm" "aarch64"))))
                    (service rootless-podman-service-type)
                    %gs-101/desktop-services))
-
-  (bootloader (bootloader-configuration
-               (bootloader grub-efi-bootloader)
-               (targets (list "/boot/efi"))
-               (keyboard-layout keyboard-layout)))
-
-  (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device (uuid
-                                  "32f6f5f3-040b-419a-a90b-11207887cd41"
-                                  'btrfs))
-                         (type "btrfs"))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "6C59-0ADD"
-                                       'fat32))
-                         (type "vfat")) %base-file-systems)))
+  (timezone "America/Sao_Paulo")
+  (users (cons* (user-account
+                 (name "gabriel")
+                 (comment "Gabriel Santos")
+                 (group "users")
+                 (home-directory "/home/gabriel")
+                 (supplementary-groups '("audio"
+                                         "cgroup" ; For podman
+                                         "video"
+                                         "wheel")))
+                %base-user-accounts)))
