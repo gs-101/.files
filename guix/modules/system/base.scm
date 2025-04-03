18530ee676895fd719c17eadf401b3c85cf0c021
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-03 10:09:48 -0300

    feat: add "podman:docker" to rootless-podman-service-type
    Use the "docker" output of podman to alias the docker command to
    podman. With this, the alias in the home configuration is not needed.
    
    This also makes the home and system configuration more independent.

--- guix/modules/system/base.scm
+++ guix/modules/system/base.scm
@@ -13,35 +13,41 @@
                      virtualization
                      xorg)
 
+(define %base-packages
+  ;; Aliases @command{docker} to @command{podman}.
+  (append (specifications->packages '("podman:docker"))
+          %base-packages))
+
 (define %base-desktop-services
   (modify-services %desktop-services
-                   (delete gdm-service-type)
-                   (guix-service-type config => (guix-configuration
-                                                 (inherit config)
-                                                 (authorized-keys
-                                                  (cons* (dotfiles-file "guix/substitute-keys/nonguix.pub")
-                                                         %default-authorized-guix-keys))
-                                                 (substitute-urls
-                                                  (cons* "https://substitutes.nonguix.org"
-                                                         %default-substitute-urls))))))
+    (delete gdm-service-type)
+    (guix-service-type config => (guix-configuration
+                                  (inherit config)
+                                  (authorized-keys
+                                   (cons* (dotfiles-file "guix/substitute-keys/nonguix.pub")
+                                          %default-authorized-guix-keys))
+                                  (substitute-urls
+                                   (cons* "https://substitutes.nonguix.org"
+                                          %default-substitute-urls))))))
+
 (define keyboard-layout
   (keyboard-layout "br" #:options '("ctrl:nocaps")))
 
 (define base-system
   (operating-system
-   (bootloader
-    (bootloader-configuration
-     (bootloader grub-efi-bootloader)
-     (keyboard-layout keyboard-layout)
-     (targets (list "/boot/efi"))))
-   (keyboard-layout keyboard-layout)
-   (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device "/dev/sda1")
-                         (type "ext4"))
-                        %base-file-systems))
-   (host-name "base-system")
-   (issue (colorize-string "
+    (bootloader
+     (bootloader-configuration
+      (bootloader grub-efi-bootloader)
+      (keyboard-layout keyboard-layout)
+      (targets (list "/boot/efi"))))
+    (keyboard-layout keyboard-layout)
+    (file-systems (cons* (file-system
+                           (mount-point "/")
+                           (device "/dev/sda1")
+                           (type "ext4"))
+                         %base-file-systems))
+    (host-name "base-system")
+    (issue (colorize-string "
  ..                             `.
  `--..```..`           `..```..--`
    .-:///-:::.       `-:::///:-.
@@ -53,22 +59,23 @@
               :+/:::-
               `-....`
 " (color CYAN)))
-   (locale "en_US.utf8")
-   (services (cons* (service cups-service-type)
-                    (service nix-service-type)
-                    (service qemu-binfmt-service-type
-                             (qemu-binfmt-configuration
-                              (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-                    (service rootless-podman-service-type)
-                    %base-desktop-services))
-   (timezone "America/Sao_Paulo")
-   (users (cons* (user-account
-                  (name "gabriel")
-                  (comment "Gabriel Santos")
-                  (group "users")
-                  (home-directory "/home/gabriel")
-                  (supplementary-groups '("audio"
-                                          "cgroup" ; For podman
-                                          "video"
-                                          "wheel")))
-                 %base-user-accounts))))
+    (locale "en_US.utf8")
+    (packages %base-packages)
+    (services (cons* (service cups-service-type)
+                     (service nix-service-type)
+                     (service qemu-binfmt-service-type
+                              (qemu-binfmt-configuration
+                               (platforms (lookup-qemu-platforms "arm" "aarch64"))))
+                     (service rootless-podman-service-type)
+                     %base-desktop-services))
+    (timezone "America/Sao_Paulo")
+    (users (cons* (user-account
+                   (name "gabriel")
+                   (comment "Gabriel Santos")
+                   (group "users")
+                   (home-directory "/home/gabriel")
+                   (supplementary-groups '("audio"
+                                           "cgroup" ; For podman
+                                           "video"
+                                           "wheel")))
+                  %base-user-accounts))))
