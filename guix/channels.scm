5e3107fa44956f1b0427ebb648fabd6bda4f5385
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-04-17 14:16:30 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "210a8d3ae4b9657ce2fdc2143688d8f8a5868dc9")
+          "b7fbe3ee7901b3462669a5128ff6a204c5a5153f")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -14,7 +14,7 @@
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "cc3ed4f2a8be8e8b233b05b1812375565ca10621")
+          "7f1208374430c3f55e3125a1cbd82772653ebb8a")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -25,7 +25,7 @@
         (url "https://github.com/gs-101/selected-guix-works.git")
         (branch "main")
         (commit
-          "e645dd5790682cc5dcf20d8d32ee203b3f2ee5c5")
+          "70405ba6f53d0d393b1942d6e480ef33fe4d1f42")
         (introduction
           (make-channel-introduction
             "5d1270d51c64457d61cd46ec96e5599176f315a4"
@@ -47,7 +47,7 @@
         (url "https://codeberg.org/guix/guix-mirror")
         (branch "master")
         (commit
-          "4bd2949cfa7a8bf5dfe66adad1a76472af09708d")
+          "cc4b392a8303788762ed9adb9c5635cf227e58a2")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
