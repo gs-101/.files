da7fbdf84e6ecac780ec0b5d165e92dac00cbf7c
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-09 15:17:13 -0300

    fix!: everything
    Too stressed to write a cohesive commit message.

--- guix/modules/system/base.scm
+++ guix/modules/system/base.scm
@@ -3,8 +3,7 @@
   #:use-module (guix colors)
   #:use-module (utils)
   #:export (base-system
-            %base-desktop-services)
-  #:export (keyboard-layout))
+            %base-desktop-services))
 
 (use-service-modules containers
                      cups
@@ -20,35 +19,32 @@
 
 (define %base-desktop-services
   (modify-services %desktop-services
-    (delete gdm-service-type)
-    (guix-service-type config => (guix-configuration
-                                  (inherit config)
-                                  (authorized-keys
-                                   (cons* (dotfiles-file "guix/substitute-keys/nonguix.pub")
-                                          %default-authorized-guix-keys))
-                                  (privileged? #f)
-                                  (substitute-urls
-                                   (cons* "https://substitutes.nonguix.org"
-                                          %default-substitute-urls))))))
-
-(define keyboard-layout
-  (keyboard-layout "br" #:options '("ctrl:nocaps")))
+                   (delete gdm-service-type)
+                   (guix-service-type config => (guix-configuration
+                                                 (inherit config)
+                                                 (authorized-keys
+                                                  (cons* (dotfiles-file "guix/substitute-keys/nonguix.pub")
+                                                         %default-authorized-guix-keys))
+                                                 (privileged? #f)
+                                                 (substitute-urls
+                                                  (cons* "https://substitutes.nonguix.org"
+                                                         %default-substitute-urls))))))
 
 (define base-system
   (operating-system
-    (bootloader
-     (bootloader-configuration
-      (bootloader grub-efi-bootloader)
-      (keyboard-layout keyboard-layout)
-      (targets (list "/boot/efi"))))
-    (keyboard-layout keyboard-layout)
-    (file-systems (cons* (file-system
-                           (mount-point "/")
-                           (device "/dev/sda1")
-                           (type "ext4"))
-                         %base-file-systems))
-    (host-name "base-system")
-    (issue (colorize-string "
+   (keyboard-layout (keyboard-layout "br" #:options '("ctrl:nocaps")))
+   (bootloader
+    (bootloader-configuration
+     (bootloader grub-efi-bootloader)
+     (keyboard-layout keyboard-layout)
+     (targets (list "/boot/efi"))))
+   (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device "/dev/sda1")
+                         (type "ext4"))
+                        %base-file-systems))
+   (host-name "base-system")
+   (issue (colorize-string "
  ..                             `.
  `--..```..`           `..```..--`
    .-:///-:::.       `-:::///:-.
@@ -60,28 +56,26 @@
               :+/:::-
               `-....`
 " (color CYAN)))
-    (locale "en_US.utf8")
-    (packages %base-packages)
-    (services (cons* (service cups-service-type)
-                     (service nix-service-type
-                              (nix-configuration
-                               (extra-config
-                                '("experimental-features = nix-command flakes"))))
-                     (service qemu-binfmt-service-type
-                              (qemu-binfmt-configuration
-                               (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-                     (service rootless-podman-service-type)
-                     %base-desktop-services))
-    (timezone "America/Sao_Paulo")
-    (users (cons* (user-account
-                   (name "gabriel")
-                   (comment "Gabriel Santos")
-                   (group "users")
-                   (home-directory "/home/gabriel")
-                   (supplementary-groups '("audio"
-                                           "cgroup" ; For podman
-                                           "video"
-                                           "wheel")))
-                  %base-user-accounts))))
-
-base-system
+   (locale "en_US.utf8")
+   (packages %base-packages)
+   (services (cons* (service cups-service-type)
+                    (service nix-service-type
+                             (nix-configuration
+                              (extra-config
+                               '("experimental-features = nix-command flakes"))))
+                    (service qemu-binfmt-service-type
+                             (qemu-binfmt-configuration
+                              (platforms (lookup-qemu-platforms "arm" "aarch64"))))
+                    (service rootless-podman-service-type)
+                    %base-desktop-services))
+   (timezone "America/Sao_Paulo")
+   (users (cons* (user-account
+                  (name "gabriel")
+                  (comment "Gabriel Santos")
+                  (group "users")
+                  (home-directory "/home/gabriel")
+                  (supplementary-groups '("audio"
+                                          "cgroup" ; For podman
+                                          "video"
+                                          "wheel")))
+                 %base-user-accounts))))
--- guix/modules/system/guix-notebook.scm
+++ guix/modules/system/guix-notebook.scm
@@ -6,31 +6,34 @@
 
 (use-service-modules networking)
 
-(define keyboard-layout
-  (keyboard-layout "br" "thinkpad" #:options '("ctrl:nocaps")))
-
 (operating-system
-  (inherit base-system)
-  (host-name "guix-notebook")
-  (initrd microcode-initrd)
-  (kernel (specification->package "linux"))
-  (mapped-devices (list (mapped-device
-                         (source (uuid
-                                  "9e7f9a43-3f83-4b73-aa14-91fc2f6125c2"))
-                         (target "root")
-                         (type luks-device-mapping))))
-  (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device "/dev/mapper/root")
-                         (type "btrfs")
-                         (dependencies mapped-devices))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "1121-4D41"
-                                       'fat32))
-                         (type "vfat"))
-                       %base-file-systems))
-  (services (cons* (service network-manager-service-type)
-                   (service ntp-service-type)
-                   (service wpa-supplicant-service-type)
-                   %base-desktop-services)))
+ (inherit base-system)
+ (host-name "guix-notebook")
+ (keyboard-layout (keyboard-layout "br" "thinkpad" #:options '("ctrl:nocaps")))
+ (bootloader
+  (bootloader-configuration
+   (bootloader grub-efi-bootloader)
+   (keyboard-layout keyboard-layout)
+   (targets (list "/boot/efi"))))
+ (initrd microcode-initrd)
+ (kernel (specification->package "linux"))
+ (mapped-devices (list (mapped-device
+                        (source (uuid
+                                 "9e7f9a43-3f83-4b73-aa14-91fc2f6125c2"))
+                        (target "root")
+                        (type luks-device-mapping))))
+ (file-systems (cons* (file-system
+                       (mount-point "/")
+                       (device "/dev/mapper/root")
+                       (type "btrfs")
+                       (dependencies mapped-devices))
+                      (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "1121-4D41"
+                                     'fat32))
+                       (type "vfat"))
+                      %base-file-systems))
+ (services (cons* (service network-manager-service-type)
+                  (service ntp-service-type)
+                  (service wpa-supplicant-service-type)
+                  %base-desktop-services)))
