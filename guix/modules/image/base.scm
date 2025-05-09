31efd5474d79a58712c07ff1e097abb89f98b0f5
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-09 18:40:22 -0300

    refactor(image/base.scm): refactoring
    * style: change indentation of nonguix signing key
    * refactor: rely on specification->package
    * fix: remove unprivileged guix
    * fix: use cons* for substitute-urls

--- guix/modules/image/base.scm
+++ guix/modules/image/base.scm
@@ -5,24 +5,18 @@
   #:use-module (guix channels)
   #:use-module (guix gexp)
   #:use-module (gnu packages)
-  #:use-module (gnu packages curl)
   #:use-module (gnu packages package-management)
-  #:use-module (gnu packages version-control)
-  #:use-module (gnu packages emacs)
   #:use-module (gnu services)
   #:use-module (gnu services base)
   #:use-module (gnu system)
   #:use-module (gnu system install)
-  #:use-module (nongnu packages linux)
   #:export (base-image))
 
 ;; <https://substitutes.nonguix.org/signing-key.pub>
 (define %signing-key
-  (plain-file "nonguix.pub" "\
-(public-key
- (ecc
-  (curve Ed25519)
-  (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+  (plain-file "nonguix.pub"
+              "(public-key (ecc (curve Ed25519)
+(q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
 
 (define %channels
   (list (channel
@@ -50,28 +44,26 @@
 
 (define base-image
   (operating-system
-    (inherit installation-os)
-    (kernel (specification->package "linux"))
-    (firmware (list linux-firmware))
-    (packages
-     (append
-      (list curl
-            git
-            emacs-no-x)
-      (operating-system-packages installation-os)))
-    (services
-     (modify-services (operating-system-user-services installation-os)
-       (guix-service-type
-        config => (guix-configuration
-                   (inherit config)
-                   (privileged? #f)
-                   (guix (guix-for-channels %channels))
-                   (authorized-keys
-                    (cons* %signing-key
-                           %default-authorized-guix-keys))
-                   (substitute-urls
-                    `(,@%default-substitute-urls
-                      "https://substitutes.nonguix.org"))
-                   (channels %channels)))))))
+   (inherit installation-os)
+   (kernel (specification->package "linux"))
+   (firmware (map specification->package '("linux-firmware")))
+   (packages
+    (append (specifications->packages '("curl"
+                                        "git"
+                                        "emacs-no-x"))
+            (operating-system-packages installation-os)))
+   (services
+    (modify-services
+     (operating-system-user-services installation-os)
+     (guix-service-type config => (guix-configuration
+                                   (inherit config)
+                                   (guix (guix-for-channels %channels))
+                                   (authorized-keys
+                                    (cons* %signing-key
+                                           %default-authorized-guix-keys))
+                                   (substitute-urls
+                                    (cons* "https://substitutes.nonguix.org"
+                                           %default-substitute-urls))
+                                   (channels %channels)))))))
 
 base-image
