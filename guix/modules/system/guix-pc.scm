0488b0aefc6e5148d8cbcb81c1cf853d5d417b3a
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-05-09 15:17:13 -0300

    fix!: everything
    Too stressed to write a cohesive commit message.

--- guix/modules/system/guix-pc.scm
+++ guix/modules/system/guix-pc.scm
@@ -4,20 +4,20 @@
   #:use-module (system base))
 
 (operating-system
-  (inherit base-system)
-  (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device (uuid
-                                  "2e34a2ed-1a32-4ef5-9c73-da47fab9183f"
-                                  'btrfs))
-                         (type "btrfs"))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "E7EB-39CF"
-                                       'fat32))
-                         (type "vfat"))
-                       %base-file-systems))
-  (firmware (map specification->package '("linux-firmware")))
-  (host-name "guix-pc")
-  (initrd microcode-initrd)
-  (kernel (specification->package "linux")))
+ (inherit base-system)
+ (file-systems (cons* (file-system
+                       (mount-point "/")
+                       (device (uuid
+                                "32f6f5f3-040b-419a-a90b-11207887cd41"
+                                'btrfs))
+                       (type "btrfs"))
+                      (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "6C59-0ADD"
+                                     'fat32))
+                       (type "vfat"))
+                      %base-file-systems))
+ (firmware (map specification->package '("linux-firmware")))
+ (host-name "guix-pc")
+ (initrd microcode-initrd)
+ (kernel (specification->package "linux")))
