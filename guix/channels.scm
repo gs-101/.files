a05d5ab5eb1b467d45137a762d650cb8711a1687
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-06-20 07:22:30 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "7152e92a13edf3e9a356e7b9b759b163aed3044c")
+          "473e5a62cd512cc0d5b4ac2c33be0bc0d0379435")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -14,7 +14,7 @@
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "413b800ad3f3f4fa13c3c85443cc14c4fce78311")
+          "0750d8f9c416deac3b7103a00a4d80d1a20640d7")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -25,7 +25,7 @@
         (url "https://github.com/gs-101/selected-guix-works.git")
         (branch "main")
         (commit
-          "93dcfdfc5128d8ab49842d2fefef142067dd8d95")
+          "1f9fa5a02ddefafe64b1333b76d2d2944785521e")
         (introduction
           (make-channel-introduction
             "5d1270d51c64457d61cd46ec96e5599176f315a4"
@@ -36,29 +36,18 @@
         (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-          "b81a1b5c8b3b7e552be41cb62645ee4514309885")
+          "8f5cc42ce3c0d229511ce1bdec38969081933114")
         (introduction
           (make-channel-introduction
             "257cebd587b66e4d865b3537a9a88cccd7107c95"
             (openpgp-fingerprint
               "2841 9AC6 5038 7440 C7E9  2FFA 2208 D209 58C1 DEB0"))))
       (channel
-        (name 'guix-xsearch)
-        (url "https://codeberg.org/baleine/guix-xsearch")
-        (branch "master")
-        (commit
-          "da262a5aac28a2935f7e569e77715ec329b07c09")
-        (introduction
-          (make-channel-introduction
-            "8035a2599cb94fca558e518c8e592cae646f5966"
-            (openpgp-fingerprint
-              "5D54 CF25 57B2 38E8 8DC1  80A2 2D22 3241 0AB7 4043"))))
-      (channel
         (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
+        (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "3030306b088abcee38be702ffd8d67448a22fbbb")
+          "352aeabf344db22a5433d783a1b7b2a7bec0e9ee")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
