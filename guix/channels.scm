19f49fd891d476dfc9d91fa629f8cd7a5de7b7a5
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2025-09-15 09:19:30 -0300

    chore(channels.scm): update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "477f283914ca771a8622e16b73d845b87c63335d")
+          "df4e6ed9fe917f004357d931e210e328e348bb38")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -14,7 +14,7 @@
         (url "https://github.com/gs-101/emacs-master.git")
         (branch "main")
         (commit
-          "737bfd201fb496d52fe09039d8103ecd8c29187d")
+          "b5ddb1dfce9a02dd96c308564c7e6f562f84cb47")
         (introduction
           (make-channel-introduction
             "568579841d0ca41a9d222a2cfcad9a7367f9073b"
@@ -36,7 +36,7 @@
         (url "https://github.com/gs-101/selected-guix-works.git")
         (branch "main")
         (commit
-          "9b3e751d252bb863e141d1cefe9ffe3f6c2ab32b")
+          "6f6599ba2a105326293394abcec8e5b22d8e4cae")
         (introduction
           (make-channel-introduction
             "5d1270d51c64457d61cd46ec96e5599176f315a4"
@@ -47,7 +47,7 @@
         (url "https://git.sr.ht/~abcdw/rde")
         (branch "master")
         (commit
-          "46a2e694a4afc3d1dbce8b751389b566df16d46a")
+          "61e29e4100112f201acf5c2b814803cc136b51b3")
         (introduction
           (make-channel-introduction
             "257cebd587b66e4d865b3537a9a88cccd7107c95"
@@ -58,7 +58,7 @@
         (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "f4ca04a820474562e51d8c27f81fe68052753154")
+          "de41613d273106b88eeed4af72c23a57cfd18442")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
