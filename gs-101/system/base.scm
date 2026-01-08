e34144e20d34572f8cd7e1fb0add66608f271c3a
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-01-07 21:51:42 -0300

    refactor(system): make qemu-binfmt-service guix-pc specific
    I don't think I'll need this in my notebook.

--- gs-101/system/base.scm
+++ gs-101/system/base.scm
@@ -11,7 +11,6 @@
   #:use-module (gnu services desktop)
   #:use-module (gnu services networking)
   #:use-module (gnu services nix)
-  #:use-module (gnu services virtualization)
   #:use-module (gnu services xorg)
   #:use-module (gnu system)
   #:use-module (gnu system accounts)
@@ -20,23 +19,37 @@
   #:use-module (gnu system shadow)
   #:use-module (guix colors)
   #:use-module (guix gexp)
-  #:export (base-system))
+  #:export (base-services
+            base-system))
 
-(define base-desktop-services
-  (modify-services %desktop-services
-    (delete gdm-service-type)
-    (guix-service-type config => (guix-configuration
-                                   (inherit config)
-                                   (authorized-keys
-                                    (cons* (local-file "substitute-keys/nonguix.pub")
-                                           (local-file "substitute-keys/moe.pub")
-                                           %default-authorized-guix-keys))
-                                   (privileged? #f)
-                                   (substitute-urls
-                                    (cons* "https://substitutes.nonguix.org"
-                                           "https://nonguix-proxy.ditigal.xyz"
-                                           "https://cache-cdn.guix.moe"
-                                           %default-substitute-urls))))))
+(define base-services
+  (cons* (service cups-service-type)
+         (service iptables-service-type) ; Podman requirement.
+         (service nix-service-type
+                  (nix-configuration
+                   (extra-config
+                    '("experimental-features = nix-command flakes"))))
+         (service rootless-podman-service-type
+                  (rootless-podman-configuration
+                   (subgids
+                    (list (subid-range (name "gabriel"))))
+                   (subuids
+                    (list (subid-range (name "gabriel"))))))
+         (modify-services
+          %desktop-services
+          (delete gdm-service-type)
+          (guix-service-type config => (guix-configuration
+                                        (inherit config)
+                                        (authorized-keys
+                                         (cons* (local-file "substitute-keys/nonguix.pub")
+                                                (local-file "substitute-keys/moe.pub")
+                                                %default-authorized-guix-keys))
+                                        (privileged? #f)
+                                        (substitute-urls
+                                         (cons* "https://substitutes.nonguix.org"
+                                                "https://nonguix-proxy.ditigal.xyz"
+                                                "https://cache-cdn.guix.moe"
+                                                %default-substitute-urls)))))))
 
 (define base-system
   (operating-system
@@ -69,23 +82,7 @@
                     podman-compose
                     qemu
                     %base-packages))
-   (services (cons* (service cups-service-type)
-                    (service iptables-service-type) ; Podman requirement.
-                    (service nix-service-type
-                             (nix-configuration
-                              (extra-config
-                               '("experimental-features = nix-command flakes"))))
-                    ;; For Guix package development.
-                    (service qemu-binfmt-service-type
-                             (qemu-binfmt-configuration
-                              (platforms (lookup-qemu-platforms "arm" "aarch64"))))
-                    (service rootless-podman-service-type
-                             (rootless-podman-configuration
-                              (subgids
-                               (list (subid-range (name "gabriel"))))
-                              (subuids
-                               (list (subid-range (name "gabriel"))))))
-                    base-desktop-services))
+   (services base-services)
    (timezone "America/Sao_Paulo")
    (users (cons* (user-account
                   (name "gabriel")
--- gs-101/system/guix-pc.scm
+++ gs-101/system/guix-pc.scm
@@ -1,25 +1,31 @@
 (define-module (gs-101 system guix-pc)
   #:use-module (gnu packages)
+  #:use-module (gnu services)
+  #:use-module (gnu services virtualization)
   #:use-module (gnu system)
   #:use-module (gnu system file-systems)
   #:use-module (gs-101 system base)
   #:use-module (nongnu system linux-initrd))
 
 (operating-system
-  (inherit base-system)
-  (file-systems (cons* (file-system
-                         (mount-point "/")
-                         (device (uuid
-                                  "32f6f5f3-040b-419a-a90b-11207887cd41"
-                                  'btrfs))
-                         (type "btrfs"))
-                       (file-system
-                         (mount-point "/boot/efi")
-                         (device (uuid "6C59-0ADD"
-                                       'fat32))
-                         (type "vfat"))
-                       %base-file-systems))
-  (firmware (map specification->package '("linux-firmware")))
-  (host-name "guix-pc")
-  (initrd microcode-initrd)
-  (kernel (specification->package "linux")))
+ (inherit base-system)
+ (file-systems (cons* (file-system
+                       (mount-point "/")
+                       (device (uuid
+                                "32f6f5f3-040b-419a-a90b-11207887cd41"
+                                'btrfs))
+                       (type "btrfs"))
+                      (file-system
+                       (mount-point "/boot/efi")
+                       (device (uuid "6C59-0ADD"
+                                     'fat32))
+                       (type "vfat"))
+                      %base-file-systems))
+ (firmware (map specification->package '("linux-firmware")))
+ (host-name "guix-pc")
+ (initrd microcode-initrd)
+ (kernel (specification->package "linux"))
+ (services (cons* (service qemu-binfmt-service-type ; For Guix package development.
+                           (qemu-binfmt-configuration
+                            (platforms (lookup-qemu-platforms "arm" "aarch64"))))
+                  base-services)))
