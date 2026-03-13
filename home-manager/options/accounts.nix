11e3c905a1562a1d570917a19c5159e500906044
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-13 15:00:09 -0300

    style(accounts.nix): use dot notation for accounts.email.accounts.disroot

--- home-manager/options/accounts.nix
+++ home-manager/options/accounts.nix
@@ -1,27 +1,25 @@
 { ... }:
 {
-  accounts.email.accounts = {
-    disroot = {
-      address = "gabrielsantosdesouza@disroot.org";
-      imap.host = "disroot.org";
-      mbsync = {
-        enable = true;
-        create = "maildir";
-      };
-      msmtp.enable = true;
-      notmuch.enable = true;
-      primary = true;
-      realName = "Gabriel Santos";
-      passwordCommand = "pass show disroot.org/gabrielsantosdesouza | head -n 1";
-      signature = {
-        text = ''
-          Gabriel Santos
-        '';
-      };
-      smtp = {
-        host = "disroot.org";
-      };
-      userName = "gabrielsantosdesouza@disroot.org";
+  accounts.email.accounts.disroot = {
+    address = "gabrielsantosdesouza@disroot.org";
+    imap.host = "disroot.org";
+    mbsync = {
+      enable = true;
+      create = "maildir";
     };
+    msmtp.enable = true;
+    notmuch.enable = true;
+    primary = true;
+    realName = "Gabriel Santos";
+    passwordCommand = "pass show disroot.org/gabrielsantosdesouza | head -n 1";
+    signature = {
+      text = ''
+        Gabriel Santos
+      '';
+    };
+    smtp = {
+      host = "disroot.org";
+    };
+    userName = "gabrielsantosdesouza@disroot.org";
   };
 }
