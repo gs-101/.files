503443b4b67cc8f1b332a08003ec1363993f602d
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-13 16:01:30 -0300

    feat(gpg-agent.nix): add pinentry program
    Add pinentry-curses as the pinentry program.

--- home-manager/options/services/gpg-agent.nix
+++ home-manager/options/services/gpg-agent.nix
@@ -1,10 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services = {
     gpg-agent = {
       enable = true;
       enableBashIntegration = true;
       enableSshSupport = true;
+      pinentry = {
+        package = pkgs.pinentry-curses;
+        program = "pinentry-curses";
+      };
     };
   };
 }
