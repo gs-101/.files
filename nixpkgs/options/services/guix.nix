7117b93db107e9de918c818b9375d984b7acdcb9
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-14 19:05:01 -0300

    feat(guix.nix): add default guix substituters

--- nixpkgs/options/services/guix.nix
+++ nixpkgs/options/services/guix.nix
@@ -5,6 +5,14 @@
       enable = true;
       substituters = {
         authorizedKeys = [
+          (builtins.fetchurl {
+            url = "https://codeberg.org/guix/guix/raw/branch/master/etc/substitutes/berlin.guix.gnu.org.pub";
+            sha256 = "0bvrs7fifrqdv8sqkxxh4jcfbvjygcxyfl5njqqkaxdc9rhm2zhg";
+          })
+          (builtins.fetchurl {
+            url = "https://codeberg.org/guix/guix/raw/branch/master/etc/substitutes/bordeaux.guix.gnu.org.pub";
+            sha256 = "0czmmcf2i4qhsxdjb3zcjcw0s12769vmgm7x34w8b0l639mbb1r7";
+          })
           # (builtins.fetchurl {
           #   url = "https://substitutes.nonguix.org/signing-key.pub";
           #   sha256 = "0000000000000000000000000000000000000000000000000000";
@@ -15,6 +23,8 @@
           })
         ];
         urls = [
+          "http://bordeaux.guix.gnu.org"
+          "http://berlin.guix.gnu.org"
           # "https://substitutes.nonguix.org"
           "https://cache-cdn.guix.moe"
         ];
