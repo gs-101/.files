19678d092fc714c237f8bb2cf1287254578ddf6f
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-16 19:23:12 -0300

    feat(nix-pc.nix): add code from hardware detection

--- nixpkgs/nix-pc.nix
+++ nixpkgs/nix-pc.nix
@@ -1,10 +1,29 @@
-{ host, ... }:
+{
+  config,
+  host,
+  lib,
+  ...
+}:
 {
   imports = [
     ./base.nix
     ./options/disko/${host}.nix
   ];
-  console.keyMap = "br";
+  boot = {
+    initrd = {
+      availableKernelModules = [
+        "ahci"
+        "sd_mod"
+        "usb_storage"
+        "usbhid"
+        "xhci_pci"
+      ];
+      kernelModules = [ ];
+    };
+    kernelModules = [ "kvm-intel" ];
+  };
+  console.keyMap = "br-abnt2";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   networking.hostName = host;
   services = {
     xserver = {
@@ -12,5 +31,6 @@
       xkb.layout = "br";
     };
   };
+  system.stateVersion = "26.05";
   time.timeZone = "America/Sao_Paulo";
 }
