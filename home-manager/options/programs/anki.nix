a22d8fafaeb8f3f7eb8f2f8a9ef4a643cfa87af5
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-16 16:40:20 -0300

    feat(anki.nix): configure anki
    - Hide top bar
    - Follow system theme and style

--- home-manager/options/programs/anki.nix
+++ home-manager/options/programs/anki.nix
@@ -1,7 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.sessionVariables = {
     ANKI_WAYLAND = 1;
   };
-  programs.anki.enable = true;
+  programs.anki = {
+    enable = true;
+    hideTopBar = true;
+    theme = "followSystem";
+    style = "native";
+  };
 }
