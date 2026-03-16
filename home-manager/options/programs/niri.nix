9a0742cfebdd07596a79bc41cfb9561e01b90125
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-16 20:26:25 -0300

    feat(niri.nix): use niri-unstable
    This version *should* support includes.

--- home-manager/options/programs/niri.nix
+++ home-manager/options/programs/niri.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.niri = {
     # TODO: migrate to settings after the include statement is added.
     enable = true;
+    package = pkgs.niri-unstable;
   };
   xdg.configFile."niri/config.kdl".text = builtins.readFile ./niri.kdl;
 }
