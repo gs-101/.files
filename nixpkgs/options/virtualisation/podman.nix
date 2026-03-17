6090f2cc9016e15226cc9eff7f1ba1eea39ea2da
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-17 08:22:03 -0300

    feat(podman.nix): add podman-compose

--- nixpkgs/options/virtualisation/podman.nix
+++ nixpkgs/options/virtualisation/podman.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation = {
     podman = {
       enable = true;
+      extraPackages = [ pkgs.podman-compose ];
       defaultNetwork.settings.dns_enabled = true;
       dockerCompat = true;
     };
