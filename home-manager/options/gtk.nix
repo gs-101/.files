a283ef1ca0021c5be6ef9f5fc48c4507e6faf5c8
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-03-17 08:22:51 -0300

    feat(gtk.nix): (try to) disable client-side decorations

--- home-manager/options/gtk.nix
+++ home-manager/options/gtk.nix
@@ -2,9 +2,17 @@
 {
   gtk = {
     enable = true;
-    gtk3.theme = {
-      name = "adw-gtk3";
-      package = pkgs.adw-gtk3;
+    gtk3 = {
+      extraConfig = {
+        gtk-decoration-layout = "menu";
+      };
+      theme = {
+        name = "adw-gtk3";
+        package = pkgs.adw-gtk3;
+      };
+    };
+    gtk4.extraConfig = {
+      gtk-decoration-layout = "menu";
     };
     iconTheme = {
       package = pkgs.adwaita-icon-theme;
