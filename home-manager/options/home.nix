c1e225c012a7495f0ed393a3c59ffb062fa76657
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-04-05 22:17:51 -0300

    feat(home.nix): add caveman and preliminary agent skills support

--- home-manager/options/home.nix
+++ home-manager/options/home.nix
@@ -1,15 +1,44 @@
 {
   config,
+  lib,
   pkgs,
   username,
   ...
 }:
+let
+  caveman = pkgs.stdenv.mkDerivation (finalAttrs: {
+    pname = "caveman";
+    version = "1.1.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "JuliusBrussee";
+      repo = "caveman";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-TYDx7QMgSVczVqNIzVHhctmD4mJWn9UE2MWchRhX77Q=";
+    };
+
+    installPhase = ''
+      runHook preInstall
+
+      install -Dm444 $src/skills/caveman/SKILL.md $out/caveman/SKILL.md
+
+      runHook postInstall
+    '';
+  });
+  skills = pkgs.symlinkJoin {
+    name = "skills";
+    paths = [
+      caveman
+    ];
+  };
+in
 {
   imports = [
     ./home/pointerCursor.nix
   ];
   home = {
     inherit username;
+    file.".agents/skills".source = skills;
     homeDirectory = "/home/${username}";
     packages = with pkgs; [
       cryptsetup
