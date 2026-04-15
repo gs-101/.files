c5c946e51a74ef0d8b2dd23fd484c77680b11a6d
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-04-15 11:06:19 -0300

    feat(vscode.nix): remove container stuff
    I never used devcontainers so these were unecessary.

--- home-manager/options/programs/vscode.nix
+++ home-manager/options/programs/vscode.nix
@@ -4,8 +4,6 @@
     enable = true;
     profiles.default = {
       extensions = with pkgs.nix-vscode-extensions.vscode-marketplace; [
-        ms-azuretools.vscode-containers
-        ms-vscode-remote.remote-containers
         cunbidun.flash-vscode
         tuttieee.emacs-mcx
         vscjava.vscode-java-pack
