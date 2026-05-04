7c5a192454143fab2ccb074f3b15fadd7248a80c
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-05-03 22:09:35 -0300

    feat: add aur ssh key

--- nixpkgs/options/sops.nix
+++ nixpkgs/options/sops.nix
@@ -16,6 +16,11 @@
     password = {
       neededForUsers = true;
     };
+    ssh_aur_key = {
+      path = "/home/${username}/.ssh/id_ed25519_aur";
+      mode = "0400";
+      owner = "${username}";
+    };
     ssh_educational_key = {
       path = "/home/${username}/.ssh/id_ed25519_educational";
       mode = "0400";
