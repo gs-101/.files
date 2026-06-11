ace7222591a8c766a92867e30260e5beb9d7dd25
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-06-11 10:10:46 -0300

    feat(fonts.nix): use monaspace instead of cascadia code

--- home-manager/options/fonts.nix
+++ home-manager/options/fonts.nix
@@ -7,7 +7,7 @@
         "Noto Color Emoji"
       ];
       monospace = [
-        "Cascadia Mono"
+        "Monaspace Argon"
         "Noto Sans Mono"
         "Noto Sans Mono CJK SC"
         "Noto Sans Mono CJK JP"
@@ -41,7 +41,7 @@
   home.packages = with pkgs; [
     aporetic
     alcarin-tengwar
-    cascadia-code
+    monaspace
     nerd-fonts.symbols-only
     noto-fonts
     noto-fonts-color-emoji
