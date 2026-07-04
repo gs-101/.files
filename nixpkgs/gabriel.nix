fc2e7798b8f7965297d459b5e34fdf3efc5521c3
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-04 08:39:00 -0300

    fix(nixpkgs/gabriel.nix): remove username comment from ssh key

--- nixpkgs/gabriel.nix
+++ nixpkgs/gabriel.nix
@@ -33,7 +33,7 @@
         hashedPasswordFile = config.sops.secrets.password.path;
         isNormalUser = true;
         openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOdceujA3tBt2vYoZE5py6f+98ZIeUdIVVj4XRwoNEO ${username}"
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOdceujA3tBt2vYoZE5py6f+98ZIeUdIVVj4XRwoNEO"
         ];
       };
     };
