e1935b5eec4fb75c351335ab6429679da2efb996
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-17 11:22:01 -0300

    feat: add initrd ssh for servers

--- nixpkgs/nix-server.nix
+++ nixpkgs/nix-server.nix
@@ -11,6 +11,7 @@
     initrd = {
       availableKernelModules = [
         "ahci"
+        "e1000e"
         "nvme"
         "sd_mod"
         "usb_storage"
--- nixpkgs/server.nix
+++ nixpkgs/server.nix
@@ -4,6 +4,16 @@
     ./general.nix
     ./options/nix/gc.nix
   ];
+  boot.initrd.network = {
+    enable = true;
+    ssh = {
+      enable = true;
+      authorizedKeys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDOdceujA3tBt2vYoZE5py6f+98ZIeUdIVVj4XRwoNEO"
+      ];
+      hostKeys = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    };
+  };
   networking = {
     useNetworkd = true;
   };
