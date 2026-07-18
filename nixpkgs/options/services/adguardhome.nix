2dc0cd1a22f468cbc1c8061c37dc81a05e688447
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-18 19:24:01 -0300

    refactor(adguardhome.nix): use config-based reverse proxy

--- nixpkgs/options/services/adguardhome.nix
+++ nixpkgs/options/services/adguardhome.nix
@@ -1,22 +1,23 @@
 {
+  config,
   host,
   ...
 }:
 {
   services = {
+    adguardhome = {
+      enable = true;
+      mutableSettings = true;
+    };
     caddy = {
       enable = true;
       virtualHosts."${host}.tailbf3a7f.ts.net" = {
         extraConfig = ''
           handle_path /adguardhome* {
-            reverse_proxy localhost:3000
+            reverse_proxy ${config.services.adguardhome.host}:${toString config.services.adguardhome.port}
           }
         '';
       };
-    };
-    adguardhome = {
-      enable = true;
-      mutableSettings = true;
     };
   };
 }
