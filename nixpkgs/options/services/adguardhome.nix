5e4853122eb50f23ecfab18224db6f011d483b34
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-18 15:19:47 -0300

    feat(adguardhome.nix): add caddy reverse proxy

--- nixpkgs/options/services/adguardhome.nix
+++ nixpkgs/options/services/adguardhome.nix
@@ -1,6 +1,19 @@
-{ ... }:
+{ host, ... }:
 {
-  services.adguardhome = {
-    enable = true;
+  services = {
+    caddy = {
+      enable = true;
+      virtualHosts."${host}.tailbf3a7f.ts.net" = {
+        extraConfig = ''
+          handle_path /adguardhome* {
+            reverse_proxy localhost:3000
+          }
+        '';
+      };
+    };
+    adguardhome = {
+      enable = true;
+      mutableSettings = false;
+    };
   };
 }
