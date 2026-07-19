463f01adc6a6cccf3691844b39d8bf2f2d758e1e
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-19 10:57:23 -0300

    feat: use blocky instead of adguardhome

--- nixpkgs/nix-server.nix
+++ nixpkgs/nix-server.nix
@@ -7,7 +7,7 @@
   imports = [
     ./server.nix
     ./options/services/anki-sync-server.nix
-    ./options/services/adguardhome.nix
+    ./options/services/blocky.nix
     ./options/services/fail2ban.nix
     ./options/services/miniflux.nix
     ./options/services/syncthing.nix
---
+++ nixpkgs/options/services/blocky.nix
@@ -0,0 +1,70 @@
+{ config, host, ... }:
+{
+  services = {
+    caddy = {
+      enable = true;
+      virtualHosts."${host}.tailbf3a7f.ts.net" = {
+        extraConfig = ''
+          handle_path /blocky* {
+            reverse_proxy  localhost:${toString config.services.blocky.settings.ports.http}
+          }
+        '';
+      };
+    };
+    blocky = {
+      enable = true;
+      settings = {
+        blocking = {
+          clientGroupsBlock = {
+            default = [
+              "hagezi"
+              "malware_filter"
+              "other"
+            ];
+          };
+          denylists = {
+            hagezi = [
+              # HaGeZi's DynDNS Blocklist
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/dyndns.txt"
+              # HaGeZi's Gambling DNS Blocklist
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/gambling.txt"
+              # HaGeZi's NSFW DNS Blocklist
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/nsfw.txt"
+              # HaGeZi's Threat Intelligence Feeds DNS Blocklist
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.txt"
+              # HaGeZi's Ultimate DNS Blocklist
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate.txt"
+            ];
+            malware_filter = [
+              # Malicious Domains Wildcard Asterisk Blocklist
+              "https://malware-filter.gitlab.io/malware-filter/urlhaus-filter-wildcard.txt"
+              # Phishing Wildcard Asterisk Blocklist
+              "https://malware-filter.gitlab.io/malware-filter/phishing-filter-wildcard.txt"
+            ];
+            other = [
+              # Blocking with ad server and tracking server hostnames
+              "https://pgl.yoyo.org/adservers/serverlist.php?showintro=1&mimetype=plaintext"
+              # how to make the internet not suck (as much)
+              "https://someonewhocares.org/hosts/hosts"
+              # StevenBlack/hosts with the gambling and porn extensions
+              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/gambling-porn/hosts"
+            ];
+          };
+        };
+        caching = {
+          minTime = "5m";
+          maxTime = "30m";
+          prefetching = true;
+        };
+        ports = {
+          http = 4000;
+        };
+        upstreams.groups.default = [
+          "https://dns.digitale-gesellschaft.ch/dns-query"
+          "https://dns.quad9.net/dns-query"
+          "https://security.cloudflare-dns.com/dns-query"
+        ];
+      };
+    };
+  };
+}
