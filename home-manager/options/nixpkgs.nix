2087d375179f0868f94f09d77cb4233d674ccd00
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-25 16:16:03 -0300

    fix(nixpkgs.nix): temporarily disable neovim-nightly-overlay

--- home-manager/options/nixpkgs.nix
+++ home-manager/options/nixpkgs.nix
@@ -4,7 +4,7 @@
     config.allowUnfree = true;
     overlays = with inputs; [
       emacs-overlay.overlays.default
-      neovim-nightly-overlay.overlays.default
+      # neovim-nightly-overlay.overlays.default
       niri-nix.overlays.niri-nix
       noctalia.overlays.default
       nur.overlays.default
