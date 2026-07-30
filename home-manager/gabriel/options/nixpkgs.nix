348de3d63727d873613c5d13ef385a4088b7ec3a
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-07-30 09:02:36 -0300

    chore: update neovim

--- home-manager/gabriel/options/nixpkgs.nix
+++ home-manager/gabriel/options/nixpkgs.nix
@@ -4,7 +4,7 @@
     config.allowUnfree = true;
     overlays = with inputs; [
       emacs-overlay.overlays.default
-      # neovim-nightly-overlay.overlays.default
+      neovim-nightly-overlay.overlays.default
       noctalia.overlays.default
       nur.overlays.default
       self.overlays.default
