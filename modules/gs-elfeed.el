022bdda16715aab3ae1e24b83c1f8238a35ff53a
Gabriel Santos <gabrielsantosdesouza@disroot.org>
2026-08-23 23:05:23 -0300

    fix: elfeed-protocol

--- modules/gs-elfeed.el
+++ modules/gs-elfeed.el
@@ -44,8 +44,8 @@ marking all current messages as read."
   (elfeed-protocol-enable)
   :custom
   (elfeed-feeds
-   '(("fever+https://gabriel@nix-server.tailbf3a7f.ts.net/miniflux/"
-      :api-url "https://nix-server.tailbf3a7f.ts.net/miniflux/fever/"
+   '(("fever+https://gabriel@miniflux.gs-101.dev/"
+      :api-url "https://miniflux.gs-101.dev/fever/"
       :use-authinfo t))))
 
 (provide 'gs-elfeed)
